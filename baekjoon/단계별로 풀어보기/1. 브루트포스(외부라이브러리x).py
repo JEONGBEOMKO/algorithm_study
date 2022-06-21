7e3fa651df40613314f801a047c5a2a2d62a87e4
JEONGBEOM KO <beom9807@gmail.com>
2022-06-21 11:31:35 +0900

    파이썬

--- baekjoon/단계별로 풀어보기/1. 브루트포스(외부라이브러리x).py
+++ baekjoon/단계별로 풀어보기/1. 브루트포스(외부라이브러리x).py
@@ -1,14 +1,26 @@
-N, M  = map(int, input().split())
-lst = list(map(int, input().split()))
+N, M = map(int, (input().split()))
 
-nlst = []
-for i in range(N):
-    for j in range(i+1, N):
+d = list(map(int, input().split()))
+
+result = 0
+Max = 0
+
+for i in range(N-2): # 3중 for문을 돌면서 겹치지 않게 범위를 지정
+    for j in range(i+1, N-1):
         for k in range(j+1, N):
-            three = lst[i] + lst[j] + lst[k]
-            if three > M:
-                continue;
-            else:
-                nlst.append(three)
+            if d[i]+d[j]+d[k] > M: # 3개의 값 더한것이 M보다 크다면 넘어감
 
-print(max(nlst))
+                continue
+            else:
+                result = d[i]+d[j]+d[k]
+                if Max <= result: #M과 가장 유사한 값은 가장 큰값이기 때문에 비교해서 큰값을 저장
+
+                    Max = result
+
+print(Max)
+
+
+# 코드 설명
+
+# 3장을 뽑아야 하기 때문에 3중 for문을 돌고 겹치지 않게 뽑기 위해 처음 for문은 N-2전까지 다음 for문은 N-2전까지 다음 for문은 i값보다 1크면서 N-1전까지 돌고 마지막 for문은 j보다 1크면서 N전까지 돌게 범위를 잡는다.
+# 합이 M보다 크다면 넘어가고 합이 M보다 작거나 같으면서 가장 큰 값을 저장해둔다음 출력해준다.
