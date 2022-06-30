ac72ced2478d595d4956f74d502f487189882fd5
JEONGBEOM KO <beom9807@gmail.com>
2022-06-30 10:02:00 +0900

    Update 수정렬하기3.cpp

--- baekjoon/단계별로 풀어보기/11. 정렬/c++/수정렬하기3.cpp
+++ baekjoon/단계별로 풀어보기/11. 정렬/c++/수정렬하기3.cpp
@@ -2,27 +2,27 @@
 
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
+	int arr[10001] = { 0, };
+	int n;
+	cin >> n;
 
-    int N;
-    cin >> N;
+	for (int i = 0; i < n; i++)
+	{
+		int input;
+		cin >> input;
+		arr[input]++;
+	}
 
-    int input[10001]={0};
-
-    for(int i=0;i<N;i++) {
-        int in;
-        cin >> in;
-        input[in]+=1;
-    }
-
-    for (int i=1; i<10001;i++) {
-        for (int j=0; j<input[i];j++) {
-            cout << i << '\n';
-        }
-    }
-
+	for (int i = 1; i < 10001; i++)
+	{
+		if (arr[i])
+			for (int j = 0; j < arr[i]; j++)
+				cout << i << '\n';
+	}
+	return 0;
 }
